efb2e81e2feda85691a86682c4b2ebfae2dea5f8
EduFreitas2 <efnascimento.filho2@gmail.com>
2024-01-31 11:29:45 -0300

    crud produto 7metodos

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -18,6 +25,10 @@ public class Categoria {
 	@NotBlank(message = "É obrigatório preencher este atributo!")
 	@Size(min = 5, max = 255, message = "Este atributo deve conter no mínimo 5 caracteres e no máximo 255")
 	private String classe;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;	
 	
 	
 	public Long getId() {
@@ -34,6 +45,14 @@ public class Categoria {
 
 	public void setClasse(String classe) {
 		this.classe = classe;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 
